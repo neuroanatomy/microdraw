9eee4e27904fd694c520632fe6587d3346755528
Denis H <github-nwl@denishovart.com>
2023-08-02 17:25:28 +0200

    WIP user settings view

--- app/views/scripts/components/UserPage.vue
+++ app/views/scripts/components/UserPage.vue
@@ -35,6 +35,29 @@
               </tbody>
             </Table>
           </Tab>
+          <Tab title="Settings">
+            <form class="embed-preferences">
+              <h3>Embed</h3>
+              <p>Limit embedding of my contents to the following hosts (1 item by line):</p>
+              <textarea placeholder="google.com" name="allowedHosts"></textarea>
+              <div class="action-buttons">
+                <button className="push-button" type="submit">Save</button>
+            </div>
+            </form>
+            <h3>Account</h3>
+            <dialog ref="removeAccountDialog" class="removeAccountDialog">
+              <form action="#" method="POST">
+                <p>
+                  Are you sure you want to delete your account?
+                </p>
+                <div class="action-buttons">
+                  <button className="push-button" value="cancel" formmethod="dialog">Cancel</button>
+                  <button className="push-button danger" type="submit" value="default">Delete account</button>
+                </div>
+              </form>
+            </dialog>
+            <button class="push-button danger" @click.prevent="showRemoveAccountDialog">Remove account</button>
+          </Tab>
         </Tabs>
       </template>
     </UserPage>
@@ -43,6 +66,7 @@
   import { ref, onMounted } from "vue";
   import { UserPage, Tabs, Tab, Table } from "nwl-components";
   const projects = ref([]);
+  const removeAccountDialog = ref(null);
   
   const props = defineProps({
       user: Object,
@@ -58,22 +82,63 @@
   onMounted(() => {
     fetchProjects();
   });
-  </script>
-  <style scoped>
-  #projects {
-    table-layout: auto;
+
+  const showRemoveAccountDialog = () => {
+    removeAccountDialog.value.showModal();
   }
-  #projects td {
-    padding: 0 4px;
-  }
-  #projects a {
-    text-decoration: none;
-  }
-  .settings {
-    opacity: 0.5;
-    transition: opacity 500ms;
-  }
-  .settings:hover {
-    opacity: 1;
-  }
-  </style>
+
+</script>
+
+<style scoped>
+#projects {
+  table-layout: auto;
+}
+#projects td {
+  padding: 0 4px;
+}
+#projects a {
+  text-decoration: none;
+}
+.settings {
+  opacity: 0.5;
+  transition: opacity 500ms;
+}
+.settings:hover {
+  opacity: 1;
+}
+.push-button {
+  border: 1px solid #ccc;
+  padding: 10px;
+  background: #222;    
+  color: white;
+}
+.push-button + .push-button {
+  margin-left: 10px;
+}
+.push-button.danger {
+  background: red;
+  font-weight: bold;
+}
+
+.removeAccountDialog {
+  background: #222;
+  box-shadow: 0 0 5px rgba(200,200,200,0.6);
+  border: 1px solid #333;
+  width: 300px;
+  padding: 20px;
+}
+
+.action-buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.embed-preferences p {
+  text-align: left;
+}
+
+.embed-preferences textarea {
+  height: 100px;
+  width: 100%;
+}
+</style>
