5c99511456252e17290fb9648d5b293c60d4df40
roberto <roberto.toro@gmail.com>
2024-05-17 16:42:09 +0200

    fix split of compound paths

--- app/public/js/tools/splitRegion.js
+++ app/public/js/tools/splitRegion.js
@@ -3,6 +3,7 @@
 
 window.ToolSplitRegion = {splitRegion: (function() {
   const tool = {
+    _tol: 1e-6, // tolerance
 
     _findHitItem: function (point) {
       const hitResult = paper.project.hitTest(point, {
@@ -48,17 +49,85 @@ window.ToolSplitRegion = {splitRegion: (function() {
       Microdraw.region.path.remove();
     },
 
-    _addRegionsFromNewPath(newPath, prevColor) {
-      newPath.remove();
-      [Microdraw.region.path] = newPath._children;
-      for( let i = 1; i < newPath._children.length; i += 1 ) {
-        const newReg = Microdraw.newRegion({
-          name: Microdraw.region.name,
-          path: newPath._children[i]
-        });
-        newReg.path.fillColor = prevColor;
+    _zeros: function (m, n) {
+      const M = [];
+      for (let i = 0; i < m; i++) {
+        M.push([]);
+        for (let j = 0; j < n; j++) {
+          M[i][j] = 0;
+        }
       }
-      paper.project.activeLayer.addChildren(newPath._children);
+
+      return M;
+    },
+
+    _intersectArea: function (childi, childj) {
+      const [path1, path2] = [
+        (new paper.Path({insert: false})).importJSON(childi),
+        (new paper.Path({insert: false})).importJSON(childj)
+      ];
+      const intersection = path1.intersect(path2);
+      const [path1Area, path2Area, intersectArea] = [path1.area, path2.area, intersection.area];
+      const [overlap1, overlap2] = [intersectArea/path1Area, intersectArea/path2Area];
+
+      return [
+        Math.abs(1 - Math.abs(overlap1)) < tool._tol ? 1 : 0,
+        Math.abs(1 - Math.abs(overlap2)) < tool._tol ? 1 : 0
+      ];
+    },
+
+    _overlaps: function (children) {
+      const overlap = tool._zeros(children.length, children.length);
+
+      for (let i = 0; i < children.length; i++) {
+        for (let j = i + 1; j < children.length; j++) {
+          [overlap[i][j], overlap[j][i]] = tool._intersectArea(children[i], children[j]);
+        }
+      }
+
+      return overlap;
+    },
+
+    // eslint-disable-next-line max-statements
+    _addRegionsFromNewPath(newPath, prevColor) {
+      const obj = JSON.parse(newPath.exportJSON());
+      const {children} = obj[1];
+      const overlap = tool._overlaps(children);
+
+      Microdraw.newRegionFlag = false;
+
+      // remove the split path (result of the divide function)
+      newPath.remove();
+
+      // include each of the parts of the split path independently
+
+      // scan the overlap matrix for each children
+      for (let i = 0; i < children.length; i++) {
+
+        // if its row is empty, then it's a new region
+        if (overlap[i].every((val) => val === 0)) {
+          let resultPath = new paper.Path({insert: false});
+          resultPath.importJSON(children[i]);
+
+          // look for a columns with overlap 1: they're the holes
+          for (let j = 0; j < children.length; j++) {
+            if (overlap[j][i] === 1) {
+              const hole = new paper.Path({insert: false});
+              hole.importJSON(children[j]).reorient();
+              resultPath = resultPath.subtract(hole);
+            }
+          }
+
+          paper.project.activeLayer.addChild(resultPath);
+
+          // add the independent part as a new region
+          Microdraw.newRegion({
+            name: Microdraw.region.name,
+            path: resultPath
+          });
+          // newReg.path.fillColor = prevColor;
+        }
+      }
     },
 
     _updateDisplay () {
@@ -66,6 +135,7 @@ window.ToolSplitRegion = {splitRegion: (function() {
       paper.view.draw();
     },
 
+    // eslint-disable-next-line max-statements
     _splitRegion (point) {
       const hitItem = tool._findHitItem(point);
       if(!hitItem) {
@@ -84,7 +154,11 @@ window.ToolSplitRegion = {splitRegion: (function() {
       const {fillColor: prevColor} = prevRegion.path;
       const newPath = Microdraw.region.path.divide(prevRegion.path);
 
+      // remove both regions from the active layer
+      tool._removeRegion(Microdraw.region);
       tool._removeRegion(prevRegion);
+
+      // add the new regions
       tool._addRegionsFromNewPath(newPath, prevColor);
     },
 
